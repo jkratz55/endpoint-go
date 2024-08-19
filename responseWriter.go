6701e8376fb53457115816ee6076516b1f4e4fc3
Joseph Kratz <4985721+jkratz55@users.noreply.github.com>
2024-08-19 09:30:32 -0400

    Implemented extra interfaces for custom response writer impl

--- responseWriter.go
+++ responseWriter.go
@@ -1,6 +1,9 @@
 package endpoint
 
 import (
+	"bufio"
+	"fmt"
+	"net"
 	"net/http"
 )
 
@@ -10,17 +13,38 @@ type responseWriter struct {
 	written int64
 }
 
-func (r responseWriter) Header() http.Header {
+func (r *responseWriter) Header() http.Header {
 	return r.w.Header()
 }
 
-func (r responseWriter) Write(bytes []byte) (int, error) {
+func (r *responseWriter) Write(bytes []byte) (int, error) {
 	n, err := r.w.Write(bytes)
 	r.written += int64(n)
 	return n, err
 }
 
-func (r responseWriter) WriteHeader(statusCode int) {
+func (r *responseWriter) WriteHeader(statusCode int) {
 	r.code = statusCode
 	r.w.WriteHeader(statusCode)
+}
+
+func (r *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	h, ok := r.w.(http.Hijacker)
+	if !ok {
+		return nil, nil, fmt.Errorf("hijacking not supported: wrapped ResponseWriter does not implement http.Hijacker")
+	}
+	return h.Hijack()
+}
+
+func (r *responseWriter) Flush() {
+	if flusher, ok := r.w.(http.Flusher); ok {
+		flusher.Flush()
+	}
+}
+
+func (r *responseWriter) Push(target string, opts *http.PushOptions) error {
+	if pusher, ok := r.w.(http.Pusher); ok {
+		return pusher.Push(target, opts)
+	}
+	return http.ErrNotSupported
 }
